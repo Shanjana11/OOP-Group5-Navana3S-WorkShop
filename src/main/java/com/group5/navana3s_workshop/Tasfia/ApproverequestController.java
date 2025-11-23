b836e9b1977a1ce1f7169d88e21781048c34470d
tasfia-tarin <tasfiatarinshammi2@gmail.com>
2025-11-23 10:08:17 +0600

    shammi2

---
+++ src/main/java/com/group5/navana3s_workshop/Tasfia/ApproverequestController.java
@@ -0,0 +1,37 @@
+package com.group5.navana3s_workshop.Tasfia;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class ApproverequestController
+{
+    @javafx.fxml.FXML
+    private TableColumn techniciancol;
+    @javafx.fxml.FXML
+    private TableColumn partnamecol;
+    @javafx.fxml.FXML
+    private TableColumn qtycol;
+    @javafx.fxml.FXML
+    private TableColumn reasoncol;
+    @javafx.fxml.FXML
+    private TableColumn statuscol;
+    @javafx.fxml.FXML
+    private TableView tableview;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void backOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void rejectOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void approveOnActionButton(ActionEvent actionEvent) {
+    }
+}
