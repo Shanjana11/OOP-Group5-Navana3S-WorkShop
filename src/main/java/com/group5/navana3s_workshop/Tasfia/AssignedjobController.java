358721a8384dac6ceed607292829794133761589
tasfia-tarin <tasfiatarinshammi2@gmail.com>
2025-11-25 09:29:03 +0600

    shammi3

---
+++ src/main/java/com/group5/navana3s_workshop/Tasfia/AssignedjobController.java
@@ -0,0 +1,46 @@
+package com.group5.navana3s_workshop.Tasfia;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class AssignedjobController
+{
+    @javafx.fxml.FXML
+    private TextField textSearch;
+    @javafx.fxml.FXML
+    private TableColumn statuscol;
+    @javafx.fxml.FXML
+    private TableColumn jobIDcol;
+    @javafx.fxml.FXML
+    private TableColumn assigneddatecol;
+    @javafx.fxml.FXML
+    private TableView jobTable;
+    @javafx.fxml.FXML
+    private TableColumn vechiclenocol;
+    @javafx.fxml.FXML
+    private TableColumn problemcol;
+    @javafx.fxml.FXML
+    private TableColumn modelcol;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void startServiceOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewdetailsOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void refreshOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void SearchOnAction(ActionEvent actionEvent) {
+    }
+}
