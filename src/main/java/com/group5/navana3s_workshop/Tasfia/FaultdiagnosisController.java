18eedc457d9dcbd1d4a95e0f95885e5c67fb4780
tasfia-tarin <tasfiatarinshammi2@gmail.com>
2025-11-25 09:33:57 +0600

    shammi4

---
+++ src/main/java/com/group5/navana3s_workshop/Tasfia/FaultdiagnosisController.java
@@ -0,0 +1,38 @@
+package com.group5.navana3s_workshop.Tasfia;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class FaultdiagnosisController
+{
+    @javafx.fxml.FXML
+    private TextField errorfield;
+    @javafx.fxml.FXML
+    private TextField vechiclefield;
+    @javafx.fxml.FXML
+    private TextField jobfield;
+    @javafx.fxml.FXML
+    private TableView tableview;
+    @javafx.fxml.FXML
+    private TableColumn errorcol;
+    @javafx.fxml.FXML
+    private TableColumn descriptioncol;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void backOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void addDiagnosisButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void saveOnActionButton(ActionEvent actionEvent) {
+    }
+}
