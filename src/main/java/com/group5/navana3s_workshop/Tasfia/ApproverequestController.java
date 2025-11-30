d94b4f46b709b57c83289612f4bafcd487067b35
tasfia-tarin <tasfiatarinshammi2@gmail.com>
2025-11-30 15:06:23 +0600

    shammi6

--- src/main/java/com/group5/navana3s_workshop/Tasfia/ApproverequestController.java
+++ src/main/java/com/group5/navana3s_workshop/Tasfia/ApproverequestController.java
@@ -1,8 +1,15 @@
 package com.group5.navana3s_workshop.Tasfia;
 
+import com.group5.navana3s_workshop.HelloApplication;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ApproverequestController
 {
@@ -24,7 +31,14 @@ public class ApproverequestController
     }
 
     @javafx.fxml.FXML
-    public void backOnActionButton(ActionEvent actionEvent) {
+    public void backOnActionButton(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/group5/navana3s_workshop/Tasfia/Approverequest.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Button signOutButton = (Button) actionEvent.getSource();
+        Stage stage = (Stage) signOutButton.getScene().getWindow();
+        stage.setScene(scene);
+
+
     }
 
     @javafx.fxml.FXML
