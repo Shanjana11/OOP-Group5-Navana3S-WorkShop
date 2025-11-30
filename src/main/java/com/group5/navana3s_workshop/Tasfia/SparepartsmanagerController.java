9ec9625db8f76ce52752628b97ec1a3c7fa7ad5c
tasfia-tarin <tasfiatarinshammi2@gmail.com>
2025-11-30 10:03:20 +0600

    shammi6

---
+++ src/main/java/com/group5/navana3s_workshop/Tasfia/SparepartsmanagerController.java
@@ -0,0 +1,38 @@
+package com.group5.navana3s_workshop.Tasfia;
+
+import javafx.event.ActionEvent;
+
+public class SparepartsmanagerController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void inventoryupdate(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void warrantyclaim(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void reorederparts(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void approverequest(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void checkstock(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void reorederdelivery(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void monthlystockreport(ActionEvent actionEvent) {
+    }
+}
